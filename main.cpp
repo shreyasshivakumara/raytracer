ae4c5bb6ac4aa1f975c41cba4391092e217dd2d5
shreyas <shrsh52@liu.se>
2024-05-22 21:48:56 +0200

    Gradient descent sphere  with green sphere as the background

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "sdltemplate.h"
 #include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
 
 // Gradient Implementation of colors
 // vec3 color(const ray& r){
@@ -11,32 +13,35 @@
 //     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.0, 0.0, 0.0);
 // }
 
-bool hit_sphere(const vec3 center, float radius, const ray& r){
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
-}
+// bool hit_sphere(const vec3 center, float radius, const ray& r){
+//     vec3 oc = r.origin() - center;
+//     float a = dot(r.direction(), r.direction());
+//     float b = 2.0 * dot(oc, r.direction());
+//     float c = dot(oc, oc) - radius*radius;
+//     float discriminant = b*b - 4*a*c;
+//     return (discriminant > 0);
+// }
 
-vec3 sphere, sphere2;
-float sphere_radius;
+//vec3 sphere, sphere2;
+//float sphere_radius;
 
-vec3 color(const ray& r){
-    if(hit_sphere(sphere, sphere_radius, r)) return vec3(1, 0, 0);
-    if(hit_sphere(sphere2, sphere_radius, r)) return vec3(0, 1, 0);
-    vec3 unit_dir = unit_vector(r.direction());
-    float t = 0.5*(unit_dir.y() + 1.0);
-    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+vec3 color(const ray& r, hitable *world) {
+    hit_record rec;
+	if(world->hit(r, 0.001, MAXFLOAT, rec)) {
+		return 0.5*vec3(rec.normal.x()+1,rec.normal.y()+1,rec.normal.z()+1);
+	} else {
+		vec3 unit_dir = unit_vector(r.direction());
+		float t = 0.5*(unit_dir.y() + 1.0);
+		return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0);
+	}
 }
 
 int main() {
 
     // Sphere
-    sphere = vec3(0, 0 , -1);
-    sphere2 = vec3(-0.5, -0.5 , 1);
-    sphere_radius = 0.25;
+    //sphere = vec3(0, 0 , -1);
+    //sphere2 = vec3(-0.5, -0.5 , 1);
+    //sphere_radius = 0.25;
 
     // Image
 
@@ -53,8 +58,12 @@ int main() {
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
-    vec3 col(0.0, 0.0, 0.0);
+    //vec3 col(0.0, 0.0, 0.0);
 
+    hitable *list[2];
+	list[0] = new sphere(vec3(0,0,-1),0.5);
+	list[1] = new sphere(vec3(0,-100.5,-1),100);
+	hitable *world = new hitable_list(list,2);
 
     for (int j = image_height -1 ; j >= 00; j--) {
         for (int i = 0; i < image_width; i++) {
@@ -62,11 +71,9 @@ int main() {
             float u = float(i)/float(image_width);
             float v = float(j)/float(image_height);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
-            //vec3 col(float(i) / float(image_width), float(j) / float(image_height), 0.2);
-            //float r = float(i) / float(image_width);
-            //float g = float(j) / float(image_height);
-            //float b = 0.2;
+            vec3 p = r.point_at_parameter(2.0);
+			vec3 col = color(r, world);
+
 
             int ir = int(255.999 * col[0]);
             int ig = int(255.999 * col[1]);
@@ -74,7 +81,7 @@ int main() {
 
             //std::cout << ir << ' ' << ig << ' ' << ib << '\n';
             sdltemplate::setDrawColor(sdltemplate::createColor(ir, ig, ib, 255));
-            sdltemplate::drawPoint(i, j);
+            sdltemplate::drawPoint(i, image_height - j);
         }
     }
     while(sdltemplate::running){
