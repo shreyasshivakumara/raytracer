8208e07e1608588dcd8595c74f78b16e08779c4f
shreyas <shrsh52@liu.se>
2024-05-23 14:44:18 +0200

    Added camera

--- main.cpp
+++ main.cpp
@@ -3,27 +3,8 @@
 #include "ray.h"
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 
-// Gradient Implementation of colors
-// vec3 color(const ray& r){
-//     vec3 unit_dir = unit_vector(r.direction());
-//     std::cout << "P3\n" << unit_dir << "\nunit_dir\n";
-//     float t = 0.5*(unit_dir.y() + 1.0);
-//     std::cout << "P3\n" << t << "\nt\n";
-//     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.0, 0.0, 0.0);
-// }
-
-// bool hit_sphere(const vec3 center, float radius, const ray& r){
-//     vec3 oc = r.origin() - center;
-//     float a = dot(r.direction(), r.direction());
-//     float b = 2.0 * dot(oc, r.direction());
-//     float c = dot(oc, oc) - radius*radius;
-//     float discriminant = b*b - 4*a*c;
-//     return (discriminant > 0);
-// }
-
-//vec3 sphere, sphere2;
-//float sphere_radius;
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
@@ -38,43 +19,35 @@ vec3 color(const ray& r, hitable *world) {
 
 int main() {
 
-    // Sphere
-    //sphere = vec3(0, 0 , -1);
-    //sphere2 = vec3(-0.5, -0.5 , 1);
-    //sphere_radius = 0.25;
-
     // Image
-
     int image_width = 800;
     int image_height = 400;
+    int ns = 100;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     sdltemplate::sdl("Ray Tracing in One Weekend", image_width, image_height);
     sdltemplate::loop();
-
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-    //vec3 col(0.0, 0.0, 0.0);
 
     hitable *list[2];
 	list[0] = new sphere(vec3(0,0,-1),0.5);
 	list[1] = new sphere(vec3(0,-100.5,-1),100);
 	hitable *world = new hitable_list(list,2);
 
+    camera cam;
+
     for (int j = image_height -1 ; j >= 00; j--) {
         for (int i = 0; i < image_width; i++) {
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48())/float(image_width);
+                float v = float(j + drand48())/float(image_height);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
 
-            float u = float(i)/float(image_width);
-            float v = float(j)/float(image_height);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 p = r.point_at_parameter(2.0);
-			vec3 col = color(r, world);
-
-
+            col /= float(ns);
             int ir = int(255.999 * col[0]);
             int ig = int(255.999 * col[1]);
             int ib = int(255.999 * col[2]);
--- vec3.h
+++ vec3.h
@@ -9,32 +9,32 @@ class vec3 {
 public:
 	vec3() {}
 	vec3(float e0, float e1, float e2) {e[0]=e0;e[1]=e1;e[2]=e2;}
-	
+
 	inline float x() const {return e[0];}
 	inline float y() const {return e[1];}
 	inline float z() const {return e[2];}
 	inline float r() const {return e[0];}
 	inline float g() const {return e[1];}
 	inline float b() const {return e[2];}
-	
+
 	inline const vec3& operator+() const {return *this;}
 	inline vec3 operator-() const {return vec3(-e[0],-e[1],-e[2]);}
 	inline float operator[](int i) const {return e[i];}
 	inline float& operator[](int i) {return e[i];}
-	
+
 	inline vec3& operator+=(const vec3 &v2);
 	inline vec3& operator-=(const vec3 &v2);
 	inline vec3& operator*=(const vec3 &v2);
 	inline vec3& operator/=(const vec3 &v2);
 	inline vec3& operator*=(const float t);
 	//inline vec3& operator/=(const float t);
-	
+
 	inline vec3& operator/=(const double t) {return *this *= 1/t;}
-	
+
 	inline float length() const {return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);}
 	inline float length_squared() const {return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];}
 	inline void make_unit_vector();
-	
+
 	float e[3];
 };
 
@@ -83,8 +83,8 @@ inline float dot(const vec3 &v1, const vec3 &v2) {
 }
 
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[1]*v2.e[2] - v1.e[2]*v2[1], 
-	-(v1.e[0]*v2.e[2] - v1.e[2]*v2[0]), 
+	return vec3(v1.e[1]*v2.e[2] - v1.e[2]*v2[1],
+	-(v1.e[0]*v2.e[2] - v1.e[2]*v2[0]),
 	v1.e[0]*v2.e[1] - v1.e[1]*v2[0]);
 }
 
